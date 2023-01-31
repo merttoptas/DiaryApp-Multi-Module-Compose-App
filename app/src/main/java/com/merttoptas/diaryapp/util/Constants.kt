27ce0f832a59e46376af40bea61d96a9a33235c5
merttoptas <merttoptas34@gmail.com>
2023-01-31 10:16:38 +0300

    id added.

---
+++ app/src/main/java/com/merttoptas/diaryapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.merttoptas.diaryapp.util
+
+/**
+ * Created by mertcantoptas on 31.01.2023
+ */
+object Constants {
+    const val MONGO_DB_ID = "diaryapp-pmlft"
+    const val CLIENT_ID = "124710705144-hm2og26fieqtedua468l33nf6n3v471i.apps.googleusercontent.com"
+}
