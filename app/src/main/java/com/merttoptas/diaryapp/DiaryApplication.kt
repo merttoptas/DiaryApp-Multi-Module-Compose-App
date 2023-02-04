1d75828a2ccfba05ba5a35c3ce2b4b139984d386
merttoptas <merttoptas34@gmail.com>
2023-02-04 23:07:05 +0300

    Hilt added.

---
+++ app/src/main/java/com/merttoptas/diaryapp/DiaryApplication.kt
@@ -0,0 +1,11 @@
+package com.merttoptas.diaryapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by mertcantoptas on 04.02.2023
+ */
+@HiltAndroidApp
+class DiaryApplication : Application() {
+}
