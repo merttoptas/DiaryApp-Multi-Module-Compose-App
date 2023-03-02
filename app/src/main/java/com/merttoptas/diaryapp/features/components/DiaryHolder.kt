36fcc53b409cbedbf06340e2e6ba88303777acfe
merttoptas <merttoptas34@gmail.com>
2023-03-02 11:15:30 +0300

    diary header added.

---
+++ app/src/main/java/com/merttoptas/diaryapp/features/components/DiaryHolder.kt
@@ -0,0 +1,65 @@
+package com.merttoptas.diaryapp.features.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.time.LocalDate
+
+/**
+ * Created by mertcantoptas on 02.03.2023
+ */
+
+@Composable
+fun DiaryDateHeader(localDate: LocalDate) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Column(horizontalAlignment = Alignment.End) {
+            Text(
+                text = String.format("%02d", localDate.dayOfMonth),
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = FontWeight.Light
+                )
+            )
+            Text(
+                text = localDate.dayOfWeek.toString().take(3),
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                    fontWeight = FontWeight.Light
+                )
+            )
+        }
+        Spacer(modifier = Modifier.width(14.dp))
+        Column(horizontalAlignment = Alignment.Start) {
+            Text(
+                text = localDate.month.toString().lowercase().replaceFirstChar { it.titlecase() },
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = FontWeight.Light
+                )
+            )
+            Text(
+                text = localDate.year.toString(),
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                    fontWeight = FontWeight.Light
+                )
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DiaryHolderPreview() {
+    DiaryDateHeader(localDate = LocalDate.now())
+}
